79c827289d82e0de0585eec02eacaa3c1254b6bf
omohokcoj <xjokerua@mail.com>
2015-03-21 21:15:16 +0200

    added role atribute to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150321120729) do
+ActiveRecord::Schema.define(version: 20150321185822) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
     t.text     "body"
     t.string   "image_url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "roles", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -36,9 +42,11 @@ ActiveRecord::Schema.define(version: 20150321120729) do
     t.datetime "updated_at"
     t.string   "first_name"
     t.string   "last_name"
+    t.integer  "role_id"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["role_id"], name: "index_users_on_role_id", using: :btree
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
+
+Role.create([{name: "user"},{name: "admin"},{name: "super_admin"}])
+
